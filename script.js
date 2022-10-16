c5c749c7c203b3d4b6dcb73c155311fbe38f8bda
andersonrbernal <110568841+andersonrbernal@users.noreply.github.com>
2022-10-16 11:18:09 -0400

    modified:   script.js

--- script.js
+++ script.js
@@ -2,68 +2,108 @@ const calculator = document.querySelector('.calculator');
 const display = document.querySelector('.calculator_display');
 const keys = document.querySelector('.calculator_keys');
 
+let firstValue = null;
+let operator = null;
+let secondValue = null;
+
 const calculate = (firstValue, operator, secondValue) => {
-    return eval(`${firstValue} ${operator} ${secondValue}`);
+    let result = null;
+    parseInt(firstValue);
+    parseInt(secondValue);
+
+    // replace the content from operator for javascript operators accordingly
+    if (operator === 'add') {
+        result = parseInt(firstValue) + parseInt(secondValue);
+    } else if (operator === 'subtract') {
+        result = firstValue - secondValue;
+    } else if (operator === 'multiply') {
+        result = firstValue * secondValue;
+    } else if (operator === 'divide') {
+        result = firstValue / secondValue;
+    }
+    
+    return result;
 }
 
 keys.onclick = (e) => {
     const key = e.target;
-    const keyContent = e.target.textContent;
+    const keyContent = key.textContent;
     const action = key.dataset.action;
-    const displaydNum = display.textContent;
-
-    if (key.matches('button')) {
-        // remove the "is-depressed" class from all operators keys
-        for (child of keys.children) {
-            if (child.className == 'key--operator is-depressed') {
-                child.classList.remove('is-depressed');
+    const displayedNum = display.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType;
+    
+    if (key.matches ('button')) {
+        // remove is-depressed class from all operator keys
+        for (childKey of keys.children) {
+            if (childKey.className === 'key--operator is-depressed') {
+                childKey.classList.remove('is-depressed');
             }
         }
         if (!action) {
-            // numeric key
-            if (displaydNum == 0) {
-                // doesn't allow the user to add more zeros at the start
-                display.textContent = keyContent;
-            } else {
-                // insert new numeric values in display
-                display.textContent += keyContent;
-            }
-        } else {
-            // operator key
-            if (
-                action == 'add' ||
-                action == 'subtract' ||
-                action == 'multiply' ||
-                action == 'divide' 
-            ) {
-                // adds a class to the pressed operator key
-                key.classList.add('is-depressed');
-                // adds a costum atribute
-                const previousKeyType = calculator.dataset.previousKeyType = 'operator';
-                
-                if (displaydNum === 0 || previousKeyType === 'operator') {
-                    // doesn't allow the user to inset a operator without a number
-                    display.textContent = display.textContent;
+            // number key
+            if (!previousKeyType) {
+                // if previousKeyType wasn't added to the calculator div
+                if (displayedNum === '0') {
+                    display.textContent = keyContent;
                 } else {
-                    // insert the operator if there is already an existing number in display
                     display.textContent += keyContent;
                 }
+            } else {
+                // if previousKeyType was added to the calculator div
+                display.textContent = keyContent; // replaces the displayed number with the pressed number
             }
-            if (action == 'decimal') {
-                display.textContent += '.';
-            } else if (action == 'clear') {
-                display.textContent = '0';
-            } else if (action == 'calculate') {
+        } else {
+            // stores the displayed value in firstValue
+            if (!previousKeyType) {
+                firstValue = displayedNum;
+            }
+            // doesn't allow calculate to be added in operator
+            if (action != 'calculate') {
+                operator = action;
+            }
 
-                const firstValue = calculator.dataset.firstValue = displaydNum;
-                const operator = calculator.dataset.operator = action;
-                const secondValue = displaydNum;
+            if (
+                action === 'add' ||
+                action === 'subtract' ||
+                action === 'multiply' ||
+                action === 'divide'
+            ) {
+                // adds a costum atribute to the calculator div that stores the previous operator type
+                calculator.dataset.previousKeyType = action;
 
-                display.textContent = calculate(firstValue, operator, secondValue);
-
+                // adds a class to the pressed operator key 
+                key.classList.add('is-depressed'); // changes the background color to lightgray
                 
+                display.textContent = displayedNum;
             }
+            if (action === 'decimal') {
+                // doesn't allow the user to add more than one dot to display
+                if (displayedNum.includes('.')) {
+                    display.textContent = displayedNum;
+                } else {
+                    display.textContent += '.';
+                }
+            }
+            if (action === 'clear') {
+                // clean content from display
+                display.textContent = '0';
+
+                // clean the stored data
+                firstValue = null;
+                operator = null;
+                secondValue = null;
+
+                // removes previousKeyType from calculator div
+                calculator.removeAttribute('data-previous-key-type');
+            }
+            if (action === 'calculate') {
+                // if there is no number or operator, the result won't be displayed
+                if (firstValue != null || operator != null || secondValue != null) {
+                    secondValue = displayedNum;
+                    display.textContent = calculate(firstValue, operator, secondValue);
+                }
+                console.log(firstValue, operator, secondValue);
+            }    
         }
     }
-
 }
