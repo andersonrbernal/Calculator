3ed02db20f80a02903608ee39becaa07e8d4c64b
andersonrbernal <110568841+andersonrbernal@users.noreply.github.com>
2022-10-16 14:25:21 -0400

    modified:   script.js

--- script.js
+++ script.js
@@ -1,66 +1,75 @@
 const calculator = document.querySelector('.calculator');
 const display = document.querySelector('.calculator_display');
 const keys = document.querySelector('.calculator_keys');
-
-let firstValue = null;
-let operator = null;
-let secondValue = null;
+const previousKeyType = calculator.dataset.previousKeyType;
 
 const calculate = (firstValue, operator, secondValue) => {
-    let result = null;
-    parseInt(firstValue);
-    parseInt(secondValue);
-
-    // replace the content from operator for javascript operators accordingly
-    if (operator === 'add') {
-        result = parseInt(firstValue) + parseInt(secondValue);
-    } else if (operator === 'subtract') {
-        result = firstValue - secondValue;
-    } else if (operator === 'multiply') {
-        result = firstValue * secondValue;
-    } else if (operator === 'divide') {
-        result = firstValue / secondValue;
-    }
+    let result = '';
     
-    return result;
+    if (operator === 'add') {
+        result = parseFloat(firstValue) + parseFloat(secondValue); 
+    } else if (operator === 'subtract') {
+        result = parseFloat(firstValue) - parseFloat(secondValue);
+    } else if (operator === 'multiply') {
+        result = parseFloat(firstValue) * parseFloat(secondValue);
+    } else if (operator === 'divide') {
+        result = parseFloat(firstValue) / parseFloat(secondValue);
+    }
+    // gets the fractional part of the number
+    const fractionalPart = result.toString().split('.')[1];
+    // verifies if the fractional part exceed the limit of 10 characters
+    if (result.toString().includes('.') && fractionalPart.length > 10) {
+        // if it does, then it returns only the ten numbers of the fractional part
+        return parseFloat(result).toFixed(10);
+    } else {
+        return result;
+    }
+
 }
 
 keys.onclick = (e) => {
     const key = e.target;
     const keyContent = key.textContent;
-    const action = key.dataset.action;
     const displayedNum = display.textContent;
     const previousKeyType = calculator.dataset.previousKeyType;
-    
-    if (key.matches ('button')) {
-        // remove is-depressed class from all operator keys
-        for (childKey of keys.children) {
-            if (childKey.className === 'key--operator is-depressed') {
-                childKey.classList.remove('is-depressed');
-            }
+    const previousKey = calculator.dataset.previousKey;
+    const action = key.dataset.action;
+
+    if (key.matches('button')) {
+        // store the type of the previous operator
+        if (action) {
+            calculator.dataset.previousKey = action;
         }
+        
+        // removes is-depressed class from all operators
+        for (child of keys.children) {
+            child.classList.remove('is-depressed');
+        }
+
+        // calculate the values before letting the user click on another operator
+        // if (firstValue && operator) {
+        //     calculate(firstValue, operator, secondValue);
+        // }
+
         if (!action) {
-            // number key
-            if (!previousKeyType) {
-                // if previousKeyType wasn't added to the calculator div
-                if (displayedNum === '0') {
-                    display.textContent = keyContent;
+            // numeric key
+            calculator.dataset.previousKeyType = 'number';
+
+            // display the pressed number key
+            if (displayedNum == 0 || previousKeyType === 'operator') {
+                // replace the zero for the pressed number key
+                if (previousKey === 'decimal') {
+                    display.textContent = displayedNum + keyContent;
                 } else {
-                    display.textContent += keyContent;
+                    display.textContent = keyContent;
                 }
             } else {
-                // if previousKeyType was added to the calculator div
-                display.textContent = keyContent; // replaces the displayed number with the pressed number
+                display.textContent = displayedNum + keyContent;
             }
+
         } else {
-            // stores the displayed value in firstValue
-            if (!previousKeyType) {
-                firstValue = displayedNum;
-            }
-            // doesn't allow calculate to be added in operator
-            if (action != 'calculate') {
-                operator = action;
-            }
+            // operator key
+            calculator.dataset.previousKeyType = 'operator';
 
             if (
                 action === 'add' ||
@@ -68,42 +77,34 @@ keys.onclick = (e) => {
                 action === 'multiply' ||
                 action === 'divide'
             ) {
-                // adds a costum atribute to the calculator div that stores the previous operator type
-                calculator.dataset.previousKeyType = action;
+                // adds is-depressed class to the pressed operator key
+                key.classList.add('is-depressed');
 
-                // adds a class to the pressed operator key 
-                key.classList.add('is-depressed'); // changes the background color to lightgray
+                calculator.dataset.firstValue = displayedNum;
+                calculator.dataset.operator = action;
                 
-                display.textContent = displayedNum;
             }
             if (action === 'decimal') {
-                // doesn't allow the user to add more than one dot to display
-                if (displayedNum.includes('.')) {
-                    display.textContent = displayedNum;
-                } else {
+                // only adds a dot if there is none
+                if (!displayedNum.includes('.')) {
                     display.textContent += '.';
                 }
             }
             if (action === 'clear') {
-                // clean content from display
+                // clears every number inserted on the calculator and screen
+                calculator.removeAttribute('data-first-value');
+                calculator.removeAttribute('data-operator');
+                
                 display.textContent = '0';
-
-                // clean the stored data
-                firstValue = null;
-                operator = null;
-                secondValue = null;
-
-                // removes previousKeyType from calculator div
-                calculator.removeAttribute('data-previous-key-type');
             }
             if (action === 'calculate') {
-                // if there is no number or operator, the result won't be displayed
-                if (firstValue != null || operator != null || secondValue != null) {
-                    secondValue = displayedNum;
-                    display.textContent = calculate(firstValue, operator, secondValue);
-                }
-                console.log(firstValue, operator, secondValue);
-            }    
+                const firstValue = calculator.dataset.firstValue;
+                const operator = calculator.dataset.operator;
+                const secondValue = displayedNum;
+
+                // display the result on the screen
+                display.textContent = calculate(firstValue, operator, secondValue);
+            }
         }
     }
 }
